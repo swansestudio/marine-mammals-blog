6ea48db894b22cc98ce6b23d62d92cad0bd0de0a
zeX514-master-win <THE451F@Gmail.com>
2023-09-06 12:25:27 +0300

    swansestudio

--- app/src/components/Home.js
+++ app/src/components/Home.js
@@ -19,13 +19,20 @@ export default function App(props) {
 
   return (
     <div className='home'>
-      {posts && posts.length === 0 
-       ? <div className="no-posts"><h4>There's No Posts Yet</h4></div>
-       : (<div className="all-posts">
-        <h4>All Posts:</h4>
-        {posts.map(post => <PostSummary {...post} deletePost={deletePost} key={post._id} />)}
+      {posts && posts.length === 0
+        ? <div className="no-posts"><h4>There's No Posts Yet</h4></div>
+        : (<div className="all-posts">
+          <h4>All Posts:</h4>
+          {posts.map(post => <PostSummary {...post} deletePost={deletePost} key={post._id} />)}
         </div>)
       }
+      <div className='footer'>
+        <p>©2023 Swansestudio</p>
+        <p>
+          This project uses the MongoDB logo, which is copyright ©MongoDB, Inc. All rights reserved. <br/>
+          <a href="https://www.mongodb.com/">Learn more about MongoDB</a>.
+        </p>
+      </div>
     </div>
   )
 }
